b40f6fb9c90b677181c09e117c90b65b1174f931
Federico <federico.baccaini@gmail.com>
2020-03-30 18:01:05 +0200

    All in one Commit (ops)

--- src/main/java/com/academy/tdd/fraction/Fraction.java
+++ src/main/java/com/academy/tdd/fraction/Fraction.java
@@ -1,42 +1,71 @@
 package com.academy.tdd.fraction;
 
-class Fraction {
+class Fraction implements Comparable<Fraction> {
+
+	private int num;
+	private int den;
 
 	Fraction(int n, int d) throws ArithmeticException {
+		if (d == 0)
+			throw new ArithmeticException();
+		this.num = n;
+		this.den = d;
 	}
-	
+
 	int getNum() {
-		return 0;
+		return num;
 	}
+
 	int getDen() {
-		return 0;
+		return den;
 	}
 
 	boolean isProper() {
-		return false;
+		return (this.num < this.den);
 	}
 
 	Fraction getReciprocal() {
-		return null;
+		return new Fraction(this.den, this.num);
 	}
 
 	static Fraction multiply(Fraction a, Fraction b) {
-		return null;
+		int newN = a.num * b.num;
+		int newD = a.den * b.den;
+		return new Fraction(newN, newD);
 	}
 
-	static Fraction multiply(Fraction b) {
-		return null;
+	static Fraction divide(Fraction a, Fraction b) {
+		return multiply(a, b.getReciprocal());
 	}
 
-	static Fraction divide(Fraction b) {
-		return null;
+	static Fraction sum(Fraction n, Fraction m) {
+		int a = n.num;
+		int b = n.den;
+		int c = m.num;
+		int d = m.den;
+
+		int newN = a * d + b * c;
+		int newD = b * d;
+		return new Fraction(newN, newD);
 	}
 
-	static Fraction sum(Fraction n) {
-		return null;
+	static Fraction subtract(Fraction n, Fraction m) {
+
+		return sum(n, m.getNegative());
+	}
+
+	private Fraction getNegative() {
+		return new Fraction(-num, den);
+	}
+
+	private boolean isNegative() {
+		return num * den < 0;
 	}
 
 	void reduce() {
+		int m = Fraction.mcd(this.num, this.den);
+		this.num /= m;
+		this.den /= m;
 	}
 
 	public static int mcd(int p, int q) {
@@ -48,7 +77,28 @@ class Fraction {
 
 	@Override
 	public String toString() {
-		return null;
+		return (num + "/" + den);
+	}
+
+	public static void print(String s) {
+		System.out.println(s);
+	}
+
+	public static void test(String test, String result, String testName) {
+		if (test.equals(result))
+			print(testName + " ok");
+		else
+			print(testName + " failed: " + test);
+	}
+
+	@Override
+	public int compareTo(Fraction f) {
+		Fraction sub = Fraction.subtract(this, f);
+		if (sub.getNum() == 0)
+			return 0;
+		if (sub.isNegative())
+			return -1;
+		return 1;
 	}
 
 }
--- src/test/java/com/academy/tdd/fraction/FractionTest.java
+++ src/test/java/com/academy/tdd/fraction/FractionTest.java
@@ -20,50 +20,107 @@ public class FractionTest {
 
 	@BeforeClass
 	public static void setupOnceAtStartup() {
-		System.out.println("Execute this only once...");
+		// System.out.println("Execute this only once...");
 	}
 
 	@Before
 	public void setupBeforeEach() {
-		System.out.println("Before each test...");
-		System.out.println("Before each test...");
+		/*
+		 * System.out.println("Before each test...");
+		 * System.out.println("Before each test...");
+		 */
 	}
 
 	@After
 	public void teardownAfterEach() {
-		System.out.println("After each test...");
+		// System.out.println("After each test...");
 	}
 
 	@After
 	public void settingNullCriticalObject() {
-		System.out.println("Setting null that");
-		System.out.println("Setting null that");
-		System.out.println("Setting null that");
+		/*
+		 * System.out.println("Setting null that");
+		 * System.out.println("Setting null that");
+		 * System.out.println("Setting null that");
+		 */
 	}
-/*Step1
+
 	@Test
 	public void shouldCreateFraction() {
 		Fraction a = new Fraction(2, 3);
-		assertEquals(2,a.getNum());
-		assertEquals(3,a.getDen());
+		assertEquals(2, a.getNum());
+		assertEquals(3, a.getDen());
 	}
-	
+
 	@Test(expected = ArithmeticException.class)
 	public void shouldnotAccept() {
 		Fraction a = new Fraction(2, 0);
 	}
-	
+
 	@Test
 	public void reciprocalTest() {
 		Fraction a = new Fraction(2, 3).getReciprocal();
-		assertEquals(3,a.getNum());
-		assertEquals(2,a.getDen());
+		assertEquals(3, a.getNum());
+		assertEquals(2, a.getDen());
 	}
-	
+
 	@Test(expected = ArithmeticException.class)
 	public void reciprocalTestWithException() {
 		Fraction a = new Fraction(0, 3).getReciprocal();
 	}
-	*/
+
+	@Test
+	public void testProduct() {
+		Fraction a = new Fraction(2, 3);
+		assertEquals("2/3", a.toString());
+		assertEquals("3/2", a.getReciprocal().toString());
+	}
+
+	@Test
+	public void testDivide() {
+		Fraction a = new Fraction(2, 3);
+		Fraction b = new Fraction(3, 2);
+		assertEquals("4/9", Fraction.divide(a, b).toString());
+	}
+
+	@Test(expected = ArithmeticException.class)
+	public void testDivideException() {
+		Fraction a = new Fraction(2, 3);
+		Fraction b = new Fraction(0, 2);
+		Fraction.divide(a, b);
+	}
+
+	@Test
+	public void testSum() {
+		Fraction a = new Fraction(2, 3);
+		Fraction b = new Fraction(3, 2);
+		assertEquals("13/6", Fraction.sum(a, b).toString());
+	}
+
+	@Test
+	public void testSubtract() {
+		Fraction a = new Fraction(5, 3);
+		Fraction b = new Fraction(2, 5);
+		assertEquals("19/15", Fraction.subtract(a, b).toString());
+	}
+
+	@Test
+	public void testReduce() {
+		Fraction a = new Fraction(3, 9);
+		a.reduce();
+		assertEquals("1/3", a.toString());
+	}
+	
+	@Test
+	public void testcompareTo() {
+		Fraction a = new Fraction(3, 9);
+		Fraction b = new Fraction(1, 3);
+		Fraction c = new Fraction(2, 3);
+		Fraction d = new Fraction(1, 4);
+
+		assertEquals(0, a.compareTo(b));
+		assertEquals(1, c.compareTo(b));
+		assertEquals(-1, d.compareTo(b));
+	}
 
 }
